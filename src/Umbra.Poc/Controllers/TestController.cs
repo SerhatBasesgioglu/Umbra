41649834a719846e0f3169506a51979d3029c512
Serhat BAŞESGİOĞLU (Uygulama Teknolojileri Yönetim Müdürlüğü) <Serhat.BASESGIOGLU@vakifbank.com.tr>
2026-03-17 09:59:36 +0300

    feat: add poc project with dummy controller

---
+++ src/Umbra.Poc/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Umbra.Poc.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<string> Get()
+    {
+        return "yo";
+    }
+}
