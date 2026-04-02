1d17c26abc93eed534cede06319981ded10e1d85
Serhat BAŞESGİOĞLU (Uygulama Teknolojileri Yönetim Müdürlüğü) <serhat.basesgioglu@vakifbank.com.tr>
2026-04-02 22:15:41 +0300

    feat: add mcp project

---
+++ src/Umbra.Mcp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithHttpTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddSingleton(_ => {
+    var client = new HttpClient();
+    return client;
+    });
+
+var app = builder.Build();
+
+app.MapGet("/api/health", ()=> Results.Ok("Healthy"));
+app.MapMcp("/mcp");
+
+app.Run();
