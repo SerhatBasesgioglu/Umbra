8e9cb24cee679ee2c8102d23aa0b3583fa1112ca
Serhat BAŞESGİOĞLU (Uygulama Teknolojileri Yönetim Müdürlüğü) <serhat.basesgioglu@vakifbank.com.tr>
2026-04-05 12:33:40 +0300

    refactor: extract pbi fetch method

--- src/Umbra.Common/Dump/AzureDevOpsService.cs
+++ src/Umbra.Common/Dump/AzureDevOpsService.cs
@@ -6,13 +6,38 @@ public class AzureDevOpsService(AzureDevOpsHttpClient client)
 {
     private readonly AzureDevOpsHttpClient _client = client;
 
-    public async Task<List<ProjectDto>> GetProjectsAsync(){
-      var response =  await _client.GetAsync<AdoList<ProjectDto>>("_apis/projects");
-      return response.Value;
+    public async Task<List<ProjectDto>> GetProjectsAsync()
+    {
+        var response = await _client.GetAsync<AdoList<ProjectDto>>("_apis/projects");
+        return response.Value;
+    }
+
+    public async Task<List<WorkItemDto>> QueryWorkItemsAsync(string query)
+    {
+        var uri = "Sandbox/_apis/wit/wiql";
+        var body = new { query };
+        var response = await _client.PostAsync<WiqlDto>(uri, body);
+        var workItems = response.WorkItems.Select(x => x.Id).ToList();
+
+        var result = GetWorkItemDetailsBatchAsync(workItems);
+        return await result;
     }
 
     public string UpdateWorkItemsAsync(int id, List<PatchDto> patches, string? project = null)
     {
         return "yo";
+    }
+
+    private async Task<List<WorkItemDto>> GetWorkItemDetailsBatchAsync(List<int> pbis)
+    {
+        var allWorkItems = new List<WorkItemDto>();
+        var uri = "Sandbox/_apis/wit/workitemsbatch";
+        foreach (var batch in pbis.Chunk(200))
+        {
+            var body = new { ids = batch, fields = new[] { "System.Id", "System.State" } };
+            var result = await _client.PostAsync<AdoList<WorkItemDto>>(uri, body);
+            allWorkItems.AddRange(result.Value);
+        }
+        return allWorkItems;
     }
 }
